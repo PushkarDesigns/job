262ef948c448599d901162d65fbce88aa709a683
Pushkar <rawatprince815@gmail.com>
2026-05-05 23:50:01 +0530

    header compelte,,,

--- frontend/src/pages/LandingPage/components/Header.jsx
+++ frontend/src/pages/LandingPage/components/Header.jsx
@@ -42,24 +42,24 @@ const Header = () => {
                     <div className="flex items-center space-x-3">
                         {isAuthenticated ? (
                             <div className="flex items-center space-x-3">
-                                <span className="">Welcome, {user?.fullName}</span>
+                                <span className="text-gray-700">Welcome, {user?.fullName}</span>
                                 <a
                                     href={
                                         user?.role === "employer"
                                             ? "/employer-dashboard"
                                             : "/find-jobs"
                                     }
-                                    className=""
+                                    className="bg-gradient-to-r from-blue-600 text-white px-2 py-2 rounded-lg font-medium hover:from-blue-700 transition-all duration-300 shadow-sm hover:shadow-md"
                                 >
                                     Dashboard
                                 </a>
                             </div>
                         ) : (
                             <>
-                                <a href="/login" className="">
+                                <a href="/login" className="text-gray-600 hover:text-gray-900 transition-colors font-medium px-4 py-2 rounded-lg hover:bg-gray-500">
                                     Login
                                 </a>
-                                <a href="/signup" className="">
+                                <a href="/signup" className="bg-gradient-to-r from-blue-600 text-white px-6 py-2 rounded-lg font-medium hover:from-blue-700 hover:to-purple-700 transtion-all duration-300 shadow-sm hover:shadow-md">
                                     Signup
                                 </a>
                             </>
