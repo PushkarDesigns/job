3690f227c39e01071d80316ef93dc54a2a2564cb
Pushkar <rawatprince815@gmail.com>
2026-06-08 23:53:58 +0530

    mange jobs handles

--- frontend/src/pages/Employer/ManageJobs.jsx
+++ frontend/src/pages/Employer/ManageJobs.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { useState, useMemo, useEffect } from "react";
+import {
+  Search,
+  Plus,
+  Edit,
+  X,
+  Trash2,
+  ChevronUp,
+  ChevronDown,
+  Users,
+} from "lucide-react";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import moment from "moment";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import DashboardLayout from '../../components/layout/DashboardLayout'
 
 const ManageJobs = () => {
   return (
-    <div>ManageJobs</div>
+    <DashboardLayout>ManageJobs</DashboardLayout>
   )
 }
 
