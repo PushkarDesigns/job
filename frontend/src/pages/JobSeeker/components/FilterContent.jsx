8a0aef1324e768a8a1b65f473996765bf011e4df
Pushkar <rawatprince815@gmail.com>
2026-06-20 23:43:31 +0530

    creating the Filter Content Component

--- frontend/src/pages/JobSeeker/components/FilterContent.jsx
+++ frontend/src/pages/JobSeeker/components/FilterContent.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import {
+  ChevronDown,
+  ChevronUp,
+} from "lucide-react";
+import { CATEGORIES, JOB_TYPES } from "../../../utils/data";
+import SalaryRangeSlider from "../../components/Input/SalaryRangeSlider";
+
+const FilterSection = ({ title, children, isExpanded, onToggle }) => {
+  return (
+    <div className="">
+      <button
+        onClick={onToggle}
+        className=""
+      >
+        {title}
+        {isExpanded ? (
+          <ChevronUp className="" />
+        ) : (
+          <ChevronDown className="" />
+        )}
+      </button>
+    </div>
+  );
+};
 
 const FilterContent = ({
-    toggleSection,
-    clearAllFilters,
-    expandedSections,
-    filters,
-    handleFilterChange,
+  toggleSection,
+  clearAllFilters,
+  expandedSections,
+  filters,
+  handleFilterChange,
 }) => {
-    return <div>FilterContent</div>;
+  return <div>FilterContent</div>;
 };
 
 export default FilterContent;
